0dfea10b7bb87ed46f1e254e9b31ddc5d2045c58
Kaik Alves <kaikalves1207@gmail.com>
2024-02-08 14:05:21 -0300

    Atualizando o Css para deixar a página de forma responsiva

---
+++ script.js
@@ -0,0 +1,6 @@
+let email = document.getElementById("email");
+let password = document.getElementById("senha");
+let entrar = document.getElementById("btn");
+let Criar = document.getElementById("btn2");
+let esqueci = document.getElementById("esqueci");
+
