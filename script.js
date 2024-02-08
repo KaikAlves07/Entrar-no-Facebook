cab9b6e54446689de57ed55c81a36e21cca93926
Kaik Alves <kaikalves1207@gmail.com>
2024-02-08 15:39:51 -0300

    Finalizando a primeira parte do Script

--- script.js
+++ script.js
@@ -2,5 +2,22 @@ let email = document.getElementById("email");
 let password = document.getElementById("senha");
 let entrar = document.getElementById("btn");
 let Criar = document.getElementById("btn2");
-let esqueci = document.getElementById("esqueci");
+var mensagemErro = document.getElementById("mensagemErro");
+
+entrar.addEventListener("click", function(event) {
+  event.preventDefault();
+  var senhaDigitada = document.getElementById("senha").value;
+  if (senhaDigitada === "01") {
+    alert("Login bem-sucedido!");
+  } else {
+    mensagemErro.style.display = "block";
+    setTimeout(function() {
+      mensagemErro.style.display = "none";
+    }, 1500); 
+  }
+});
+
+Criar.addEventListener("click",function(){
+    window.location.href = "https://www.facebook.com/campaign/landing.php?";
+})
 
